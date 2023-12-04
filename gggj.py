16942209900d4383f7dc18e9ba75e344218d5212
Sara Nicole Arroyave Muñiz <sara.aprendizidi@interaseo.com.co>
2023-12-04 16:25:37 -0500

    ahorcad3o

--- gggj.py
+++ gggj.py
@@ -6,9 +6,7 @@ while len(letras_erroneas) < len(simbolos): # pasos 7 y 8
     mostrar_tablero(tablero, letras_erroneas) # paso 3
     letra = pedir_letra(tablero, letras_erroneas) # paso 4
     procesar_letra(letra, palabra, tablero, letras_erroneas) #paso 5
-    if comprobar_palabra(tablero): # paso 6
-        print('¡Enhorabuena, lo has logrado!')
-        break
+
 else:
     print(f'¡Lo siento! ¡Has perdido! La palabra a adivinar era {palabra}.')
     mostrar_escenario(len(letras_erroneas)) #paso 7
