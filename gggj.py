5e0c702749aa73e0fee976aaf6a5c9f96cbfbd75
Sara Nicole Arroyave Muñiz <sara.aprendizidi@interaseo.com.co>
2023-12-04 16:23:46 -0500

    ahorcado

--- gggj.py
+++ gggj.py
@@ -1,10 +1,16 @@
-b = 48
-a = 12
+diccionario = ['casa', 'pescado', 'calamar', 'monigote', 'codigopiton']
 
-print(type(a))
+tablero, palabra, letras_erroneas = inicializar_juego(diccionario) # paso 1
+while len(letras_erroneas) < len(simbolos): # pasos 7 y 8
+    mostrar_escenario(len(letras_erroneas)) # paso 2
+    mostrar_tablero(tablero, letras_erroneas) # paso 3
+    letra = pedir_letra(tablero, letras_erroneas) # paso 4
+    procesar_letra(letra, palabra, tablero, letras_erroneas) #paso 5
+    if comprobar_palabra(tablero): # paso 6
+        print('¡Enhorabuena, lo has logrado!')
+        break
+else:
+    print(f'¡Lo siento! ¡Has perdido! La palabra a adivinar era {palabra}.')
+    mostrar_escenario(len(letras_erroneas)) #paso 7
 
-x='Profe la Buena'
-
-print(x)
-
-print("estamos en la Espera de saber como se actualiza")
+mostrar_tablero(tablero, letras_erroneas)
